3714d124285ec196bea6beab63c9e754c86635ec
loserStar <362527240@qq.com>
2018-01-25 14:52:33 +0800

    UrlParam.js

---
+++ UrlParam.js
@@ -0,0 +1,12 @@
+/**
+ * 获取url之后的参数
+ */
+function getQueryString(name)
+{
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null){
+    	 return  unescape(r[2]);
+     }
+     return "";
+}
