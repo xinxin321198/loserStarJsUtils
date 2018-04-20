20911298b1e30bec58bb1883e9e21df280e512d6
loserStar <362527240@qq.com>
2018-04-20 16:07:12 +0800

    add ajax demo

---
+++ jqueryUtil/ajaxUtils.js
@@ -0,0 +1,74 @@
+/**
+ * post提交json数据
+ */
+function postJson(url,data,dataType,callBack){
+	$.ajax({ 
+        type:"POST", 
+        url:url, 
+        dataType:dataType,      
+        contentType:"application/json; charset=utf-8",               
+        data:JSON.stringify(data), 
+        success:callBack
+     }); 
+}
+
+/**
+ * form表单转为json提交
+ * @param {} url 
+ * @param {*} formSelector 
+ * @param {*} dataType 
+ * @param {*} callBack 
+ */
+function submitFormToJson(url,formSelector,dataType,callBack){
+        postJson(url, formToJson(formSelector), callBack, 'json');
+}
+
+/**
+ * form转为json对象(原始方式)
+ * @param {*} formSelector 
+ */
+function formToJson(formSelector){
+        var data = {};
+        var formArray = $(formSelector).serializeArray();
+        for (let index = 0; index < formArray.length; index++) {
+            const element = formArray[index];
+            data[element.name] = element.value;
+        }
+        return data;
+}
+
+/**
+ * form转为json对象(插件方式https://github.com/marioizquierdo/jquery.serializeJSON)
+ * @param {*} formSelector 
+ */
+function formToJson_jquerySerializeJSON(formSelector){
+        return $(formSelector).serializeJSON();
+}
+
+/**
+ * object转为json
+ * http://www.json.org/提供了一个json.js,这样ie8(兼容模式),ie7和ie6就可以支持JSON对象以及其stringify()和parse()方法； 
+可以在https://github.com/douglascrockford/JSON-js上获取到这个js，一般现在用json2.js。
+ * @param {*} obj 
+ */
+function objToJson(obj){
+        return JSON.stringify(obj); //可以将json对象转换成json对符串 
+}
+
+/**
+ * json字符串转为object
+ * http://www.json.org/提供了一个json.js,这样ie8(兼容模式),ie7和ie6就可以支持JSON对象以及其stringify()和parse()方法； 
+可以在https://github.com/douglascrockford/JSON-js上获取到这个js，一般现在用json2.js。
+ * @param {*} str 
+ */
+function jsonToObj(str){
+        return JSON.parse(str); //jQuery.parseJSON(jsonstr),可以将json字符串转换成json对象 
+}
+/**
+ * javascript支持的eval方式
+ * eval可以将json字符串转换成json对象,注意需要在json字符外包裹一对小括号 
+注：ie8(兼容模式),ie7和ie6也可以使用eval()将字符串转为JSON对象，但不推荐这些方式，这种方式不安全eval会执行json串中的表达式。 
+ */
+function jsonToObj_eval(str){
+        return eval('(' + str + ')');
+}
