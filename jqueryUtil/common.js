d6d429313d3397928de36be631cdd20cfa3e7aff
loserStar <362527240@qq.com>
2018-05-24 22:35:06 +0800

    增加浏览器版本判断

--- jqueryUtil/common.js
+++ jqueryUtil/common.js
@@ -1,4 +1,59 @@
 /**
+ * 检查IE浏览器版本
+ * @returns
+ */
+function IEVersion() {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器  
+    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器  
+    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    if(isIE) {
+        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+        reIE.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+        if(fIEVersion == 7) {
+            return 7;
+        } else if(fIEVersion == 8) {
+            return 8;
+        } else if(fIEVersion == 9) {
+            return 9;
+        } else if(fIEVersion == 10) {
+            return 10;
+        } else {
+            return 6;//IE版本<=7
+        }   
+    } else if(isEdge) {
+        return 'edge';//edge
+    } else if(isIE11) {
+        return 11; //IE11  
+    }else{
+        return -1;//不是ie浏览器
+    }
+}
+
+/**
+ * 检查浏览器版本
+ * @returns
+ */
+function BorwserVersion(){
+	var isIE = IEVersion();
+	if(isIE==-1){//非IE
+		var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
+		var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器  
+		var isFF = userAgent.indexOf("Firefox") > -1; //判断是否Firefox浏览器  
+		var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1; //判断是否Safari浏览器  
+		var isChrome = userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1; //判断Chrome浏览器  
+		if (isFF) {  return "FF";}  
+		if (isOpera) {  return "Opera";}  
+		if (isSafari) {  return "Safari";}  
+		if (isChrome) { return "Chrome";}  
+		if (isEdge) { return "Edge";} 
+	}else{
+		return isIE;
+	}
+}
+
+/**
  * 把某个checkbox里选中的值以逗号分隔放入第二个参数的元素的value上
  * @param checkBoxName
  * @param strSelector
