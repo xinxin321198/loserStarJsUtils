1c862abd5c40327ca1e47ac8bc1dba42e0b8feba
loserStar <362527240@qq.com>
2018-11-07 15:44:41 +0800

    fix

--- jqueryUtil/AuditWindow2.js
+++ jqueryUtil/AuditWindow2.js
@@ -37,74 +37,82 @@
 	//2.使用组件的initWindow方法，生成审核框对象
     auditWindow1 = AuditWindow2.initWindow("auditWindow","#auditWindowOk","#auditWindowCancel");
     auditWindow2 = AuditWindow2.initWindow("auditWindow2","#auditWindowOk2","#auditWindowCancel2");
-    //3.在某个事件方法中，弹出需要弹出的框，传入两个回调方法，以及在哪个页面元素上弹出审核框，没有的话传null,默认弹出位置是浏览器1/3宽度，1/2高度的地方
+    //3.在某个事件方法中，使用全局对象里的open方法，弹出需要弹出的框，传入两个回调方法，以及在哪个页面元素上弹出审核框，没有的话传null,默认弹出位置是浏览器1/3宽度，1/2高度的地方
     var elment  = $("#要弹出的位置所在的元素的id");
     auditWindow1.open(function callBackOK(){alert("点击确定")},function callBackCancel(){alert("点击取消")});
  */
 var AuditWindow2 = {};
-AuditWindow2.initWindow= function (windowId,okBtnSelector,cancelBtnSelector) {
-	var tempWindow = {};
-	// 记录下初始化时候的参数，便于其它方法调用
-	tempWindow.windowId = windowId;
-	tempWindow.okBtnSelector = okBtnSelector;
-	tempWindow.cancelBtnSelector = cancelBtnSelector;
-	tempWindow.open = AuditWindow2.open;
-	// 初始化window
-	var window = $("#"+windowId);
-	var browserWidth = $(window).width();
-	var browserHeight = $(window).height();
-	window.jqxWindow({
-	    position: { x: browserWidth/3, y: browserHeight/2} ,
-		isModal:true,
-	    autoOpen: false,
-	    draggable: true,
-	    resizable: true,
-	    showCollapseButton: false,
-	    maxHeight: 500,
-	    maxWidth: 1000,
-	    minHeight: 200,
-	    minWidth: 200,
-	    height: 300,
-	    width: 700
-	});
-  // 初始化确定按钮
-  $(okBtnSelector).jqxButton({
-    height : 30,
-    width : 80
-  });
-  // 初始化取消按钮
-  $(cancelBtnSelector).jqxButton({
-    height : 30,
-    width : 80
-  });
-  
-  return tempWindow
+AuditWindow2.initWindow = function (windowId, okBtnSelector, cancelBtnSelector) {
+    var tempWindow = {};
+    // 记录下初始化时候的参数，便于其它方法调用
+    tempWindow.windowId = windowId;
+    tempWindow.okBtnSelector = okBtnSelector;
+    tempWindow.cancelBtnSelector = cancelBtnSelector;
+    tempWindow.open = AuditWindow2.open;
+    // 初始化window
+    var window = $("#" + windowId);
+    var browserWidth = $(window).width();
+    var browserHeight = $(window).height();
+    window.jqxWindow({
+        position: {
+            x: browserWidth / 3,
+            y: browserHeight / 2
+        },
+        isModal: true,
+        autoOpen: false,
+        draggable: true,
+        resizable: true,
+        showCollapseButton: false,
+        maxHeight: 500,
+        maxWidth: 1000,
+        minHeight: 200,
+        minWidth: 200,
+        height: 300,
+        width: 700
+    });
+    // 初始化确定按钮
+    $(okBtnSelector).jqxButton({
+        height: 30,
+        width: 80
+    });
+    // 初始化取消按钮
+    $(cancelBtnSelector).jqxButton({
+        height: 30,
+        width: 80
+    });
+
+    return tempWindow
 }
 
 /**
  * 打开审核窗口，传入确定和取消按钮的事件方法回调
  */
-AuditWindow2.open = function(okCallBack,cancelCallBack,element){
-	var tempWindow  = this;
-	//为确定按钮绑定传入的点击事件
-  $(tempWindow.okBtnSelector).on('click', function(e) {
-    if(okCallBack!=undefined){
-      okCallBack();
+AuditWindow2.open = function (okCallBack, cancelCallBack, element) {
+    var tempWindow = this;
+    //为确定按钮绑定传入的点击事件
+    $(tempWindow.okBtnSelector).on('click', function (e) {
+        if (okCallBack != undefined) {
+            okCallBack();
+        }
+        $("#" + tempWindow.windowId).jqxWindow('close');
+    });
+
+    //为取消按钮绑定传入的取消事件
+    $(tempWindow.cancelBtnSelector).on('click', function (e) {
+        if (cancelCallBack != undefined) {
+            cancelCallBack();
+        }
+        $("#" + tempWindow.windowId).jqxWindow('close');
+    });
+    if (undefined != element && null != element && "" != element) {
+        var offset = $(element).offset();
+        $("#" + tempWindow.windowId).jqxWindow({
+            position: {
+                x: offset.left,
+                y: offset.top
+            }
+        });
     }
-    $("#"+tempWindow.windowId).jqxWindow('close');
-  });
-  
-  //为取消按钮绑定传入的取消事件
-  $(tempWindow.cancelBtnSelector).on('click', function(e) {
-    if(cancelCallBack!=undefined){
-      cancelCallBack();
-    }
-    $("#"+tempWindow.windowId).jqxWindow('close');
-  });
-  if(undefined!=element&&null!=element&&""!=element){
-	  var offset = $(element).offset();
-	  $("#"+tempWindow.windowId).jqxWindow({position: { x: offset.left, y: offset.top}}); 
-  }
-  //执行打开窗口操作
-  $("#"+tempWindow.windowId).jqxWindow('open'); 
+    //执行打开窗口操作
+    $("#" + tempWindow.windowId).jqxWindow('open');
 }
