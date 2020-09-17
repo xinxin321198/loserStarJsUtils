aec0d481b41d5fe348dd7fa60f12b0b30cce962d
xinxin321198 <362527240@qq.com>
2020-09-17 18:34:47 +0800

    扩展

--- jqueryUtil/loserStarSweetAlertUtils.js
+++ jqueryUtil/loserStarSweetAlertUtils.js
@@ -24,3 +24,46 @@ loserStarSweetAlertUtils.alert = function(title,content,type){
 	if(!content){content="";}
 	swal(title,content,type);
 }
+
+loserStarSweetAlertUtils.confirm = function(title,content,okCallback,cancelCallback){
+	swal({
+	    title: title,
+	    text: content,
+	    icon: "warning",
+	    buttons: ["取消","确定"],
+	    dangerMode: true,
+	    closeOnClickOutside: false
+	  }).then(function(will){
+		  if(will){
+			  if(okCallback){
+				  okCallback();
+			  }
+		  }else{
+			  if(cancelCallback){
+				  cancelCallback();
+			  }
+		  }
+	  });
+}
+
+loserStarSweetAlertUtils.prompt = function(title, content, placeholder, okCallback,
+		cancelCallback) {
+	swal({
+		title : title,
+		text : content,
+		icon : "warning",
+		button:"确定",
+		content : {
+			element : "input",
+			attributes : {
+				placeholder : placeholder,
+				type : "text",
+			}
+		},
+	}).then(function(will) {
+		if (okCallback) {
+			okCallback(will);
+		}
+	});
+	;
+}
